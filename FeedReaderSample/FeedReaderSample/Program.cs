cff6da370fc0eeee0c0a2311405b2dc6da9facda
k2491p <fukuda.dev10@gmail.com>
2022-04-18 22:43:43 +0900

    add rss_sample_code

---
+++ FeedReaderSample/FeedReaderSample/Program.cs
@@ -0,0 +1,25 @@
+﻿using CodeHollow.FeedReader;
+
+static class Program
+{
+    /// <summary>
+    /// メインメソッド
+    /// </summary>
+    static void Main()
+    {
+        string url = @"https://kyoko-np.net/index.xml";
+        var feedTask = FeedReader.ReadAsync(url);
+        var feed = feedTask.Result;
+
+        // ヘッダー情報
+        Console.WriteLine("Feed Title: " + feed.Title);
+        Console.WriteLine("Feed Description: " + feed.Description);
+        Console.WriteLine("Feed Images: " + feed.ImageUrl);
+
+        // 明細情報
+        foreach (var item in feed.Items)
+        {
+            Console.WriteLine(item.Title + " - " + item.Link);
+        }
+    }
+}
